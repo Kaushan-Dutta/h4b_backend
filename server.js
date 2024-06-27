ef25b5139e35e5a348a6a4491802ae1cc4adc6b0
Kaushan-Dutta <kaushandutta5@gmail.com>
2024-06-28 00:14:00 +0530

    initial commit

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express=require('express')
 const mongoose=require('mongoose')
 const http=require('http');
 const cors=require('cors')
+const Razorpay = require('razorpay');
 
 const app=express()
 const server=http.createServer(app)
@@ -43,6 +44,29 @@ app.use('/api/admin',userExists,isAdmin,require('./src/routes/admin'));
 app.use('/api/user',userExists,isUser,require('./src/routes/user'));
 
 app.use('/api/placeorder',require('./src/routes/public'));
+
+app.post("/payment/orders", async (req, res) => {
+    try {
+        const instance = new Razorpay({
+            key_id: 'rzp_test_njkhQ67c5Bgwlt',
+            key_secret: 'HNIdzDkYyFUeWgZaYatrgaiO',
+        });
+
+        const options = {
+            amount: 20000, 
+            currency: "INR",
+            receipt: "receipt_order_74394",
+        };
+
+        const order = await instance.orders.create(options);
+
+        if (!order) return res.status(500).send("Some error occured");
+
+        res.json(order);
+    } catch (error) {
+        res.status(500).send(error);
+    }
+});
 
 const PORT=process.env.PORT || 5000
 server.listen(PORT,()=>console.log(`Server running on port ${PORT}`))
